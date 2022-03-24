58d815eaede942a79e97624c64879f49f0fd94bd
MahiET <meazalemma2020@gmail.com>
2022-03-24 14:18:49 +0000

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,35 +1,17 @@
 #!/usr/bin/env bash
 # web server for deployment
-sudo apt-get -y update
+sudo apt-get update
 sudo apt-get -y install nginx
-
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
-
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Best School
+  </body>
+</html>" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
-sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
-
-sudo service nginx restart#!/usr/bin/env bash
-# web server for deployment
-sudo apt-get -y update
-sudo apt-get -y install nginx
-
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tBest School\n\t</body>\n</html>" >/data/web_static/releases/test/index.html
-
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-new_str="location /hbnb_static {\nalias /data/web_static/current;\n}"
-sudo sed -i "/# Only/ i $new_str" /etc/nginx/sites-enabled/default
-
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "26i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
