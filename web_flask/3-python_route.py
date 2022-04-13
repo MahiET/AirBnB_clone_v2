672d048226aef31b3f64402ed9ba3c1566f283b6
MahiET <meazalemma2020@gmail.com>
2022-04-13 14:06:48 +0000

    application must be listening

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,29 +4,31 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """Start a basic Flask web application"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Adding a specific route /hbnb"""
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/c/<string:text>', strict_slashes=False)
-def c_text(text=None):
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
     """Dynamic inputed text: replace _ for space and show text"""
-    return "C {}".format(text.replace('_', ' '))
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<string:text>', strict_slashes=False)
-def python_text(text='is_cool'):
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is_cool"):
     """Dynamic inputed text: replace _ for space and show text"""
-    return "Python {}".format(text.replace('_', ' '))
+     text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
