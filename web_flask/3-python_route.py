72ace2771710381cb4a7c6ff1996ba6e6d948b3d
MahiET <meazalemma2020@gmail.com>
2022-04-15 08:38:37 +0000

    Python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Start a basic Flask web application"""
+    """"Display 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Adding a specific route /hbnb"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Dynamic inputed text: replace _ for space and show text"""
+    """Displays 'C' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes."""
+
     text = text.replace("_", " ")
     return "C {}".format(text)
 
@@ -25,10 +35,13 @@ def c(text):
 @app.route("/python/", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is_cool"):
-    """Dynamic inputed text: replace _ for space and show text"""
+    """Displays 'Python' followed by the value of <text>.
+
+    Replaces any underscores in <text> with slashes."""
+
      text = text.replace("_", " ")
     return "Python {}".format(text)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
