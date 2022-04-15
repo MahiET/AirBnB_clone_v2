dbc1aa3001da6b842e02580a59b78f9543c848f8
MahiET <meazalemma2020@gmail.com>
2022-04-15 08:58:07 +0000

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
+"""
+
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """Start a basic Flask web application"""
     return 'Hello HBNB!'
 
@@ -42,4 +54,4 @@ def first_template(n=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
