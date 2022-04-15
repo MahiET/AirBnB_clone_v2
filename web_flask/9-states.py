2885a904dd6bc3379a55935d999f4995063d2c10
MahiET <meazalemma2020@gmail.com>
2022-04-15 09:42:47 +0000

    Cities by states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states: HTML page with a list of all State objects.
+    /states/<id>: HTML page displaying the given state with <id>.
+"""
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
@@ -13,7 +20,7 @@ def appcontext_teardown(self):
 
 
 @app.route('/states', strict_slashes=False)
-def state_info():
+def states():
     """Display a HTML page inside the tag BODY"""
     return render_template('7-states_list.html',
                            states=storage.all(State))
@@ -26,6 +33,5 @@ def state_id(id=None):
                            states=storage.all(State)
                            .get('State.{}'.format(id)))
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
