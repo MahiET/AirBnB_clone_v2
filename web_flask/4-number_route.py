654ea92a5225664813b0725d4b87ce175a2417ca
MahiET <meazalemma2020@gmail.com>
2022-04-15 08:49:44 +0000

    Is it a number

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     """Start a basic Flask web application"""
     return 'Hello HBNB!'
 
@@ -35,4 +44,4 @@ def only_digits_dynamic(n=None):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
